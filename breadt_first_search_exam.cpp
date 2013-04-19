6282daa26582769a8cd1371d516762a78f111864
Daniel Pedroza <danpe91@gmail.com>
2013-04-19 13:02:59 -0500

    added weight calculation in kruskal and programs given in the exam with inputs

---
+++ breadt_first_search_exam.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <queue>
+
+#define qi queue<int>
+
+using namespace std;
+
+bool contiene_elemento(int ** arista, int index, int * recorrido) {
+
+	for (int i = 1; i <= recorrido[0]; ++i)
+	{
+		if ((int)arista[index * 2 + 0] == recorrido[i] || (int)arista[index * 2 + 1] == recorrido[i])
+			return true;
+	}
+
+	return false;
+}
+
+bool es_arbol(int ** aristas, int numero_de_aristas, int * recorrido) {
+
+	int contador_de_aristas = 0;
+
+	for (int i = 0; i < numero_de_aristas; ++i)
+	{
+		if (contiene_elemento(aristas, i,recorrido)) {
+			contador_de_aristas++;
+		}
+	}
+	
+	return ((contador_de_aristas + 1) == recorrido[0])? true : false;
+
+}
+
+void generar_matriz_de_adyacencia(int * matriz_de_adyacencia, int ** aristas, int vertices, int numero_de_aristas) {
+
+	for (int i = 0; i < vertices; ++i) {
+		for (int j = 0; j < vertices; ++j) {
+
+			matriz_de_adyacencia[i * vertices + j] = 0;
+		}
+	}
+
+	for(int i = 0; i < numero_de_aristas; i++) {
+		
+		int x = (int)aristas[i * 2 + 0];
+		int y = (int)aristas[i * 2 + 1];
+
+		matriz_de_adyacencia[(x-1) * vertices + (y-1)] = 1;
+		matriz_de_adyacencia[(y-1) * vertices + (x-1)] = 1;
+	}
+
+}
+
+void breadth_first_search(int * recorrido, int ** matriz_de_adyacencia, int size, int initial_index) {
+
+	int i;
+	bool visitados[size];
+	qi cola;
+
+	for (int i = 0; i <= size; ++i)
+	{
+		recorrido[i] = visitados[i] = 0;
+	}
+
+	cola.push(initial_index);
+	visitados[initial_index] = 1;
+
+	for (int k = 0; k < size, !cola.empty(); ++k) {
+		i = cola.front();
+		cola.pop();
+		visitados[i]=1;
+		recorrido[++recorrido[0]] = i + 1;
+
+		for (int j = 0; j < size; ++j) {
+
+			if ((int)matriz_de_adyacencia[i*size+j] && (!visitados[j])) {
+				
+				cola.push(j);
+				visitados[j] = 1;
+			}
+		}
+	}
+
+}
+
+int main() {
+
+	cout << "Recorrido primero en anchura" << endl;
+	
+	int size = 0, vertices = 0, numero_de_aristas = 0, search_index = 0, contador_componentes = 0, contador_arboles = 0;
+
+	//cout << "Número de vertices y aristas: ";
+	cin >> vertices >> numero_de_aristas;
+	
+	int aristas[numero_de_aristas][2], matriz_de_adyacencia[vertices][vertices], recorrido[vertices];
+
+	for (int i = 0; i < numero_de_aristas; ++i)
+	{
+		//cout << "Introduce arista " << i + 1 <<": ";
+		cin >> aristas[i][0] >> aristas[i][1];
+	}
+
+	generar_matriz_de_adyacencia((int *)matriz_de_adyacencia, (int **)aristas, vertices, numero_de_aristas);
+
+
+	while (search_index < vertices) {
+
+		breadth_first_search(recorrido, (int **)matriz_de_adyacencia, vertices, search_index);
+		
+		if (es_arbol((int **)aristas, numero_de_aristas, (int *)recorrido)) {
+			contador_arboles++;
+		}
+
+		cout << endl << "Recorrido: " << endl << '\t';
+		for (int i = 1; i <= recorrido[0]; ++i) {
+
+			search_index = (recorrido[i] > search_index) ? recorrido[i] : search_index;
+			cout << recorrido[i] << ((i != (recorrido[0])) ? ' ': '\n');
+		}
+
+	}
+	
+	cout << ((recorrido[0] == vertices) ? ("Es conexo") : ("No es conexo")) << endl;
+	cout << (( ((numero_de_aristas + 1) == vertices) && (recorrido[0] == vertices)) ? ("Es arbol") : ("No es arbol")) << endl;
+
+}
--- kruskal_exam.cpp
+++ kruskal_exam.cpp
@@ -78,42 +78,69 @@ int calcularpeso(const arista * arbol, const int size) {
 	return weigth;
 }
 
+bool arista_igual(const arista first, const arista second) {
+
+	return first.origin == second.origin && first.destiny == second.destiny && first.weigth == second.weigth;
+}
+
+bool contiene_arista(arista * arbol, const int size, arista elemento) {
+
+	for (int i = 0; i < size; ++i) {
+		
+		if (arista_igual(arbol[i], elemento)) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+void generar_aristas_segundo_arbol(arista * aristas, const int numero_aristas, arista * arbol, const int aristas_arbol, arista * grafo_resultante) {
+
+	int index = 0;
+
+	for (int i = 0; i < numero_aristas; ++i)
+	{
+		if (!contiene_arista(arbol, aristas_arbol, aristas[i])) {
+			grafo_resultante[index++] = aristas[i];
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	int numero_aristas = 0, numero_vertices = 0;
+	int numero_aristas = 0, numero_vertices = 0, numero_aristas_segundo_grafo = 0;
 
 	cin >> numero_vertices >> numero_aristas;
 
-	arista aristas[numero_aristas], arbol_minimo[numero_vertices - 1];
+	numero_aristas_segundo_grafo = numero_aristas - (numero_vertices - 1);
+	arista aristas[numero_aristas], arbol_minimo[numero_vertices - 1], aristas_segundo_grafo[numero_aristas_segundo_grafo], segundo_arbol_minimo[numero_vertices - 1];
 
 	for (int i = 0; i < numero_aristas; ++i)
 	{
 		cin >> aristas[i].origin >> aristas[i].destiny >> aristas[i].weigth;
 	}
 	
+	cout << "Grafo inicial\n";
 	printaristas(aristas, numero_aristas);
 	size_t structs_len = sizeof(aristas) / sizeof(struct arista);
 	qsort (aristas, structs_len, sizeof(struct arista), compare);
+	cout << "Grafo ordenado\n";
 	printaristas(aristas, numero_aristas);
 	generararbol_expansionminima(arbol_minimo, aristas, numero_vertices);
+	cout << "Arbol de expansion minima, peso: " << calcularpeso(arbol_minimo, numero_vertices - 1) << endl;
 	printaristas(arbol_minimo, numero_vertices - 1);
-	cout << calcularpeso(arbol_minimo, numero_vertices - 1) << endl;
+
+	generar_aristas_segundo_arbol(aristas, numero_aristas, arbol_minimo, numero_vertices - 1, aristas_segundo_grafo);
+	
+	cout << "Segundo grafo inicial\n";
+	printaristas(aristas_segundo_grafo, numero_aristas_segundo_grafo);
+	size_t structs_len2 = sizeof(aristas_segundo_grafo) / sizeof(struct arista);
+	qsort (aristas_segundo_grafo, structs_len2, sizeof(struct arista), compare);
+	cout << "Segundo grafo ordenado\n";
+	printaristas(aristas_segundo_grafo, numero_aristas_segundo_grafo);
+	generararbol_expansionminima(segundo_arbol_minimo, aristas_segundo_grafo, numero_vertices);
+	cout << "Segundo arbol de expansion minima, peso: " << calcularpeso(segundo_arbol_minimo, numero_vertices - 1) << endl;
+	printaristas(segundo_arbol_minimo, numero_vertices - 1);
 
 }
-
-// 6 7
-// 1 2 1
-// 1 4 2
-// 2 3 3
-// 2 4 1
-// 3 4 3
-// 4 5 1
-// 5 6 4
-
-
-// Salida
-// 1 2 1
-// 2 4 1
-// 2 3 3
-// 4 5 1
-// 5 6 4
